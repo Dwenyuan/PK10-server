5c10cd7a865dc299d01fe2031e5e80667fb4fe0d
liushao121 <liushao1990@sina.com>
2016-08-16 02:54:30 +0800

    修改请求方式

--- src/main/java/com/pk10/util/UserInfoFormWeChat.java
+++ src/main/java/com/pk10/util/UserInfoFormWeChat.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.Map;
 
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.fluent.Request;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
