3a4ee9b475774030413a1bd6f77c6d8d075e5754
“dengfengdecao” <dengfengdecao@gmail.com>
2016-10-11 16:05:58 +0800

    隔离代理商和管理员登录

--- src/main/java/com/pk10/control/LoginFilter.java
+++ src/main/java/com/pk10/control/LoginFilter.java
@@ -4,7 +4,6 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
